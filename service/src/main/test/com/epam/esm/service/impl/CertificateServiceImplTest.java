5a4a81c73afc93d6a60b70f0748fe892ad89ca3e
Dzakhveisha <Dzakhveisha@gmail.com>
2022-01-13 21:17:00 +0300

    example test

---
+++ service/src/main/test/com/epam/esm/service/impl/CertificateServiceImplTest.java
@@ -0,0 +1,13 @@
+package com.epam.esm.service.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CertificateServiceImplTest {
+
+    @Test
+    void findById() {
+        assertEquals(1,1);
+    }
+}
