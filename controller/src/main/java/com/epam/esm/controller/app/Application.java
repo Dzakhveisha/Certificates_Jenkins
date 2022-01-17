4d468fca19a52194bc68400237ef425235434f55
Dzakhveisha <Dzakhveisha@gmail.com>
2022-01-17 20:45:39 +0300

    edit app

--- controller/src/main/java/com/epam/esm/controller/app/Application.java
+++ controller/src/main/java/com/epam/esm/controller/app/Application.java
@@ -3,15 +3,23 @@ package com.epam.esm.controller.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = {"com.epam.esm.**"})
 @EntityScan("com.epam.esm.dao.model")
 @EnableJpaRepositories("com.epam.esm.dao")
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(new Class[]{Application.class}, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application)
+    {
+        return application.sources(Application.class);
     }
 
 }
